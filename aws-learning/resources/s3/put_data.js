4d989ab801fc13e1212dd707e66bf16d9e3d9353
tindaominh <tin.daominh@gmail.com>
2021-07-13 15:34:39 +0700

    put data s3

---
+++ aws-learning/resources/s3/put_data.js
@@ -0,0 +1,17 @@
+var { s3 } = require('./s3');
+
+var params = {
+    Bucket: 's3-bucket-sample-1',
+    Key: 'test-image.png'
+};
+
+s3.putObject(params, function (err, data) {
+    if (err) {
+        console.error('Unable put file. Error: ', JSON.stringify(err, null, 2));
+    } else {
+        console.log('Put file succeeded.');
+        console.log(data);
+    }
+});
+
+// get file - http://localhost:4566/s3-bucket-sample-1/test-image.png
