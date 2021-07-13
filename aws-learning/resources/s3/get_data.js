a9770d3cd56b70996ad521ea42e1f1419443634f
tindaominh <tin.daominh@gmail.com>
2021-07-13 14:56:33 +0700

    get object s3

--- aws-learning/resources/s3/get_data.js
+++ aws-learning/resources/s3/get_data.js
@@ -3,14 +3,14 @@ var { s3, fs } = require('./s3')
 var params = {
    Bucket: 's3-bucket-sample-1',
    Key: 'json-data.json',
+   Range: 'bytes=0-9' // get 10 item 
 };
 
 s3.getObject(params, (err, res) => {
    if (err) {
       console.error('Unable get data. Error: ' + JSON.stringify(err, null, 2));
    } else {
-      var data = JSON.stringify(res, null, 2);
-      fs.writeFileSync('json_data_s3.json', data);
+      fs.writeFileSync('data.json', JSON.stringify(res, null, 2));
       console.log('Get data succeeded.');
    }
 });
